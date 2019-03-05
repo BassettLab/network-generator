dcf45db0ec97e95f3e87c601cd15da47f25f4b60
Harang Ju <harangju@gmail.com>
2019-03-05 10:18:45 -0500

    clean up generate.m

--- generate.m
+++ generate.m
@@ -2,22 +2,10 @@ function network = generate(varargin)
 %GENERATE
 %   returns a struct with adjacency matrix in 'A' and other parameters
 %   See README for usage
-network = struct(...
-    'A', [],...
-    'topology', ''...
-    );
+network = struct;
 for pair = reshape(varargin,2,[])
     network.(lower(pair{1})) = pair{2};
 end
-switch network.topology
-    case 'erdosrenyi'
-        network.A = net.generators.erdosrenyi(network);
-    case 'wattsstrogatz'
-        network.A = net.generators.wattsstrogatz(network);
-    case 'randomgeometric'
-        network.A = net.generators.randomgeometric(network);
-    otherwise
-        error(['Topology ' network.topology ' undefined.'])
-end
+network.A = feval(['net.generators.' network.topology], network);
 end
 
