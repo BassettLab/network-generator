6be0ef8f6cb54efaba40325d2b16d27953735fdd
Harang Ju <harangju@gmail.com>
2019-03-05 11:09:26 -0500

    Update py_graph2adjmat.m
    iterate over edges; can handle parallel edges

--- +helper/py_graph2adjmat.m
+++ +helper/py_graph2adjmat.m
@@ -1,20 +1,15 @@
 function adjmat = py_graph2adjmat(g)
-%UNTITLED2 
+%py_graph2adjmat(g) 
 %   g: Python networkx graph
 % returns
 %   adjmat: adjacency matrix from g
+% NOTE
+%   function throws out edge attributes
 nodes = cell(py.list(g.nodes));
-sources = py.list(g.adj);
 adjmat = zeros(length(nodes));
-for i = 1 : length(nodes)
-    s = double(sources{i});
-    ts = cellfun(@double,cell(py.list(g.adj{s})));
-    for j = 1 : length(ts)
-        val = g.adj{s}{ts(j)};
-        val = cellfun(@double,cell(py.list(val))) + 1;
-        val(isempty(val)) = 1;
-        adjmat(ts(j)+1,s+1) = val;
-    end
+edges = cell(py.list(g.edges));
+for i = 1 : length(edges)
+    edge = cellfun(@double,cell(edges{i}));
+    adjmat(edge(1)+1,edge(2)+1) = adjmat(edge(1)+1,edge(2)+1) + 1;
 end
 end
-
