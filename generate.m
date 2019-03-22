799f9799d783f6000b9a4c698d5eb95a35d46f67
Harang Ju <wngkfkd94@gmail.com>
2019-03-22 09:27:55 -0400

    remove 'topology' as parameter

--- generate.m
+++ generate.m
@@ -1,14 +1,14 @@
-function network = generate(varargin)
+function network = generate(topology,varargin)
 %GENERATE
+%   topology (string)
 %   returns a struct with adjacency matrix in 'A' and other parameters
 %   See README for usage
 network = struct;
 for pair = reshape(varargin,2,[])
     network.(lower(pair{1})) = pair{2};
 end
-data = feval(['net.generators.' network.topology], network);
+data = feval(['net.generators.' topology], network);
 for field = fieldnames(data)'
     network.(field{1}) = data.(field{1});
 end
 end
-
